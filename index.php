2415e9690d2b9153bae2e3b117025c21bf2930de
tconroy <tdc5536@rit.edu>
2013-12-17 23:55:17 -0500

    initial push -- homepage html/css mostly complete.
    needs some tidying up and eventually integration with the backend but
    it is a start.

---
+++ index.php
@@ -0,0 +1,137 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Foundation | Welcome</title>
+  <link rel="stylesheet" href="css/foundation.css" />
+  <link rel="stylesheet" href="css/foundation-icons.css" />
+  <link rel="stylesheet" href="css/custom.css" />
+  <script src="js/modernizr.js"></script>
+</head>
+<body>
+
+<nav class="top-bar" data-topbar>
+
+  <ul class="title-area">
+    <li class="name">
+      <h1><a href="#"></a></h1>
+    </li>
+    <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+  </ul>
+
+  <section class="top-bar-section">
+    <!-- Right Nav Section -->
+    <ul class="right">
+      <li class="has-form">
+        <div class="row collapse">
+          <div class="large-8 small-9 columns">
+            <input type="text" placeholder="search">
+          </div>
+          <div class="large-4 small-3 columns">
+            <a href="#" class="alert button expand  fi-magnifying-glass size-18"></a>
+          </div>
+        </div>
+      </li>
+      <li class="has-dropdown">
+        <a href="#">Help</a>
+        <ul class="dropdown">
+          <li><a href="#">F.A.Q</a></li>
+          <li><a href="#">Guides</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Feedback</a></li>
+        </ul>
+      </li>
+    </ul>
+
+    <!-- Left Nav Section -->
+    <ul class="left">
+    </ul>
+  </section>
+  <section class="top-bar-section" >
+    <dl class="sub-nav">
+      <dd class="active"><a href="#">Home</a></dd>
+      <dt>|</dt>
+      <dd><a href="#">Search</a></dd>
+      <dt>|</dt>
+      <dd><a href="#">Profiles</a></dd>
+      <dt>|</dt>
+      <dd><a href="#">Projects</a></dd>
+    </dl>
+  </section>
+</nav>
+
+
+  <div class="row">
+    <div class="large-12 columns center-box">
+     <div class="panel trans-container">
+       <div class="row">
+        
+        <div class="large-4 medium-4 columns home-blurb">
+        <div class="text-center">
+          <img  src="img/icons/chat-128.png" alt="">
+        </div> 
+         <p>
+            <a href="http://foundation.zurb.com/docs">Foundation Documentation</a>
+            <br>
+            Looking for a project to work on? Need some help but don't know anyone with 
+            the skills needed? Or maybe you just want to network? With our new searching 
+            tool finding people or getting help becomes so much easier.
+          </p>
+          <a href="" class="button small">Search</a>
+       </div>
+       <div class="large-4 medium-4 columns home-blurb">
+         <div class="text-center">
+          <img src="img/icons/stationery-02-128.png" alt="">
+         </div>
+         <p>
+            <a href="http://github.com/zurb/foundation">Foundation on Github</a>
+            <br>
+            Looking for a project to work on? Need some help but don't know anyone with 
+            the skills needed? Or maybe you just want to network? With our new searching 
+            tool finding people or getting help becomes so much easier.
+         </p>
+         <a href="" class="button small">Search</a>
+       </div>
+       <div class="large-4 medium-4 columns home-blurb">
+          <div class="text-center">
+            <img src="img/icons/Data-Find-128.png" alt="">
+          </div>
+         <p>
+          <a href="http://twitter.com/foundationzurb">@foundationzurb</a>
+          <br>
+            Looking for a project to work on? Need some help but don't know anyone with 
+            the skills needed? Or maybe you just want to network? With our new searching 
+            tool finding people or getting help becomes so much easier.
+          </p>
+            <a href="" class="button small">Search</a>
+       </div>        
+     </div>
+   </div>
+ </div>
+</div>
+
+<footer>
+  <div class="row">
+    <div class="twelve columns footer-container">
+      <ul class="footer-ul">
+        <li>Copyright &copy; <?php echo date('Y');?> Rochester Institute of Technology. <small>All Rights Reserved</small></li>
+      </ul>      
+      <ul class="footer-ul">
+        <li><a href="">Copyright Infringement</a></li>
+        <li>|</li>
+        <li><a href="">Privacy Statement</a></li>
+        <li>|</li>
+        <li><a href="">Disclaimer</a></li>
+      </ul>
+    </div>  
+  </div>
+</footer>
+
+<script src="js/jquery.js"></script>
+<script src="js/foundation.min.js"></script>
+<script>
+$(document).foundation();
+</script>
+</body>
+</html>
